f9e6f2e2adb38acaa0febd9055b9ac8735dc90f3
vanbumi <jsp.dio@gmail.com>
2016-04-12 13:59:33 +0700

    add validation contact us

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,10 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :name, :email, :nohp, :comment
-  validates_format_of :email :with => /^(+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  # validates_format_of :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  # validates_numericality_of   :value
+  #                             :only_integer =>
+  validates :nohp, numericality: { only_integer: true }
+
 
 end
