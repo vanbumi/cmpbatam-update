83583d3efab6576b22e627ff24eda0d4e0ea05b5
vanbumi <jsp.dio@gmail.com>
2016-04-26 03:43:46 +0700

    new update

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,6 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :name, :email, :nohp, :comment
-  # validates_format_of :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  # validates_numericality_of   :value
-  #                             :only_integer =>
   validates :nohp, numericality: { only_integer: true }
 
 
