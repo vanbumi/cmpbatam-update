e4f0d06a943ac94a09af98b183c75bd575f7b6c2
vanbumi <jsp.dio@gmail.com>
2016-04-27 23:44:50 +0700

    add form validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ActiveRecord::Base
 
-	# validates		 	:images, presence: true
-	has_attachments 	:images, maximum: 1, accept: [:jpg, :png, :gif]
+	validates		 				:name, :slug, :location, :status, :property_type, :price, :images, presence: true
+	has_attachments 		:images, maximum: 1, accept: [:jpg, :png, :gif]
 	# has_attachments 	:images, maximum: 5, accept: [:jpg, :png, :gif]
 
 end
