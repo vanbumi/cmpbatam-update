ce3b84147377f3f2577d402f0818633849d88432
vanbumi <jsp.dio@gmail.com>
2016-04-27 22:44:30 +0700

    max upload for 1 only

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,19 +1,8 @@
 class Property < ActiveRecord::Base
 
 	# validates		 	:images, presence: true
-	has_attachments 	:images, accept: [:jpg, :png, :gif]
+	has_attachments 	:images, maximum: 1, accept: [:jpg, :png, :gif]
 	# has_attachments 	:images, maximum: 5, accept: [:jpg, :png, :gif]
-
-	# def self.search(search)
-	#   if search
-	#     where("name LIKE ?", "%#{search}%")
-	#     # where('location LIKE ?', "%#{search}%")
-	#   else
-	#     all
-	#   end
-	# end
-
-
 
 end
 
